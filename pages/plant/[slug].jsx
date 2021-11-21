e5d42172062be6723d28362ff6c1f79416606998
Savo Kos <savo.kos.sk@gmail.com>
2021-11-21 15:41:47 +0100

    .

--- pages/plant/[slug].jsx
+++ pages/plant/[slug].jsx
@@ -1,9 +1,4 @@
 import { useSelector } from 'react-redux';
-import Head from 'next/head';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { updateInitialPlants, updatePlants } from 'redux/filtersSlice';
 import Navigation from '@components/Navigation';
 import styled from 'styled-components';
 import Hero from '@components/PlantDetails/Hero';
@@ -13,17 +8,6 @@ import RelatedProducts from '@components/PlantDetails/RelatedProducts';
 import Footer from '@components/Footer';
 
 function PlantDetails({ slug }) {
-  const dispatch = useDispatch();
-
-  useEffect(
-    () =>
-      axios.get('/plants.json').then((res) => {
-        dispatch(updateInitialPlants(res.data));
-        dispatch(updatePlants(res.data));
-      }),
-    [dispatch]
-  );
-
   const { initialPlants } = useSelector((state) => state.filters);
   const plant = initialPlants.filter(
     (plant) => plant.name.toLowerCase().split(' ').join('-') === slug
@@ -33,14 +17,6 @@ function PlantDetails({ slug }) {
 
   return (
     <>
-      <Head>
-        <title>Greenery | {plant?.name}</title>
-        <meta name='description' content={plant?.description} />
-        <link
-          href={'https://greenery.savokos.com/plants' + slug}
-          rel='canonical'
-        />
-      </Head>
       <Navigation />
       <S.Container>
         <Hero plant={plant} />
